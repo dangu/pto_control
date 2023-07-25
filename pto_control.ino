3c9f8af14ed8dfde604ba7532b35c8356e4e5e88
Daniel Gullberg <daniel_gullberg@hotmail.com>
2023-07-25 16:50:18 +0200

    With relay

--- pto_control.ino
+++ pto_control.ino
@@ -1,23 +1,68 @@
+/****************************************************************************************************************************
+  ISR_Modify_PWM.ino
+  For AVR-based boards  (UNO, Nano, Mega, 32U4, 16U4, etc. )
+  Written by Khoi Hoang
 
-#define USE_TIMER_1 true
+  Built by Khoi Hoang https://github.com/khoih-prog/AVR_Slow_PWM
+  Licensed under MIT license
+  
+  TCNTx - Timer/Counter Register. The actual timer value is stored here.
+  OCRx - Output Compare Register
+  ICRx - Input Capture Register (only for 16bit timer)
+  TIMSKx - Timer/Counter Interrupt Mask Register. To enable/disable timer interrupts.
+  TIFRx - Timer/Counter Interrupt Flag Register. Indicates a pending timer interrupt.
+
+  Now even you use all these new 16 ISR-based timers,with their maximum interval practically unlimited (limited only by
+  unsigned long miliseconds), you just consume only one  RP2040-based timer and avoid conflicting with other cores' tasks.
+  The accuracy is nearly perfect compared to software timers. The most important feature is they're ISR-based timers
+  Therefore, their executions are not blocked by bad-behaving functions / tasks.
+  This important feature is absolutely necessary for mission-critical tasks.
+*****************************************************************************************************************************/
+
+#define T_START    3000L  // [ms] Start duty cycle will be hold for this time interval
+
+
+
+#if ( defined(__AVR_ATmega644__) || defined(__AVR_ATmega644A__) || defined(__AVR_ATmega644P__) || defined(__AVR_ATmega644PA__)  || \
+        defined(ARDUINO_AVR_UNO) || defined(ARDUINO_AVR_NANO) || defined(ARDUINO_AVR_MINI) ||    defined(ARDUINO_AVR_ETHERNET) || \
+        defined(ARDUINO_AVR_FIO) || defined(ARDUINO_AVR_BT)   || defined(ARDUINO_AVR_LILYPAD) || defined(ARDUINO_AVR_PRO)      || \
+        defined(ARDUINO_AVR_NG) || defined(ARDUINO_AVR_UNO_WIFI_DEV_ED) || defined(ARDUINO_AVR_DUEMILANOVE) || defined(ARDUINO_AVR_FEATHER328P) || \
+        defined(ARDUINO_AVR_METRO) || defined(ARDUINO_AVR_PROTRINKET5) || defined(ARDUINO_AVR_PROTRINKET3) || defined(ARDUINO_AVR_PROTRINKET5FTDI) || \
+        defined(ARDUINO_AVR_PROTRINKET3FTDI) )
+  #define USE_TIMER_1     true
+#else          
+  #define USE_TIMER_3     true
+#endif
 
 // These define's must be placed at the beginning before #include "AVR_Slow_PWM.h"
 // _PWM_LOGLEVEL_ from 0 to 4
 // Don't define _PWM_LOGLEVEL_ > 0. Only for special ISR debugging only. Can hang the system.
-#define _PWM_LOGLEVEL_ 3
+#define _PWM_LOGLEVEL_      3
 
-#define USING_MICROS_RESOLUTION true
+#if (_PWM_LOGLEVEL_ > 3)
+  #if USE_TIMER_1
+    #warning Using Timer1
+  #elif USE_TIMER_1
+    #warning Using Timer3
+  #endif
+#endif
+  
+#define USING_MICROS_RESOLUTION       true    //false
+
+// Default is true, uncomment to false
+//#define CHANGING_PWM_END_OF_CYCLE     false
 
 // To be included only in main(), .ino with setup() to avoid `Multiple Definitions` Linker Error
 #include "AVR_Slow_PWM.h"
 
-#ifndef LED_BUILTIN
-#define LED_BUILTIN 13
-#endif
+#define STATE_OFF           HIGH
+#define STATE_ON            LOW
+
+#define OUTPUT_PIN    2
 
 // Don't change these numbers to make higher Timer freq. System can hang
-#define HW_TIMER_INTERVAL_MS 0.1f
-#define HW_TIMER_INTERVAL_FREQ 10000L
+#define HW_TIMER_INTERVAL_MS        0.1f
+#define HW_TIMER_INTERVAL_FREQ      10000L
 
 volatile uint32_t startMicros = 0;
 
@@ -26,95 +71,164 @@ AVR_Slow_PWM ISR_PWM;
 
 //////////////////////////////////////////////////////
 
-void TimerHandler() {
+void TimerHandler()
+{ 
   ISR_PWM.run();
 }
 
 //////////////////////////////////////////////////////
 
-#define USING_PWM_FREQUENCY true
+#define USING_PWM_FREQUENCY     false //true
 
 //////////////////////////////////////////////////////
 
-// You can assign pins here. Be careful to select good pin to use or crash
-uint32_t PWM_Pin = LED_BUILTIN;
+// You can assign pins here. Be carefull to select good pin to use or crash
+uint32_t PWM_Pin    = OUTPUT_PIN;
 
 // You can assign any interval for any timer here, in Hz
-float PWM_Freq = 8.0f;
+float PWM_Freq   = 8.0f;   //1.0f;
+
+
+// You can assign any interval for any timer here, in microseconds
+uint32_t PWM_Period1 = 1000000 / PWM_Freq;
+
 
 // You can assign any duty_cycle for any PWM here, from 0-100
-float PWM_DutyCycle1 = 20.0;
-// You can assign any duty_cycle for any PWM here, from 0-100
-float PWM_DutyCycle2 = 90.0;
+float PWM_DutyCycle  = 60.0f;     //50.0f;
 
-/**@brief Change duty cycle of output
-
-@param duty_cycle [float] duty cycle
-*/
-static void changeDutyCycle(float dutyCycle) {
-  static int channelNum;
-  static bool firstRun;
-  if (!firstRun) {
-    ISR_PWM.deleteChannel((unsigned)channelNum);
-
-  } else {
-    firstRun = false;
-  }
-  channelNum = ISR_PWM.setPWM(PWM_Pin, PWM_Freq, dutyCycle);
-}
+// Channel number used to identify associated channel
+int channelNum;
 
 ////////////////////////////////////////////////
 
-void setup() {
+void runStartRamp()
+{
+
+      changePWM(PWM_DutyCycle);
+      delay(T_START);
+      ITimer1.detachInterrupt();
+
+      pinMode(OUTPUT_PIN, OUTPUT);
+      digitalWrite(OUTPUT_PIN, STATE_ON);
+}
+
+
+void setup()
+{
   Serial.begin(115200);
-  while (!Serial)
-    ;
+  while (!Serial);
 
-  delay(2000);
+ // delay(2000);
 
-  Serial.print(F("\nStarting ISR_Changing_PWM on "));
-  Serial.println(BOARD_NAME);
+  Serial.print(F("\nStarting ISR_Modify_PWM on ")); Serial.println(BOARD_NAME);
   Serial.println(AVR_SLOW_PWM_VERSION);
-  Serial.print(F("CPU Frequency = "));
-  Serial.print(F_CPU / 1000000);
-  Serial.println(F(" MHz"));
+  Serial.print(F("CPU Frequency = ")); Serial.print(F_CPU / 1000000); Serial.println(F(" MHz"));
 
   // Timer0 is used for micros(), millis(), delay(), etc and can't be used
   // Select Timer 1-2 for UNO, 1-5 for MEGA, 1,3,4 for 16u4/32u4
   // Timer 2 is 8-bit timer, only for higher frequency
   // Timer 4 of 16u4 and 32u4 is 8/10-bit timer, only for higher frequency
 
+#if USING_HW_TIMER_INTERVAL_MS
+
+#if USE_TIMER_1
+
   ITimer1.init();
 
   // Using ATmega328 used in UNO => 16MHz CPU clock ,
 
-  if (ITimer1.attachInterrupt(HW_TIMER_INTERVAL_FREQ, TimerHandler)) {
-    Serial.print(F("Starting  ITimer1 OK, micros() = "));
-    Serial.println(micros());
-  } else
+  if (ITimer1.attachInterruptInterval(HW_TIMER_INTERVAL_MS, TimerHandler))
+  {
+    Serial.print(F("Starting  ITimer1 OK, micros() = ")); Serial.println(micros());
+  }
+  else
     Serial.println(F("Can't set ITimer1. Select another freq. or timer"));
+    
+#elif USE_TIMER_3
 
-  //   ISR_PWM.setPWM(PWM_Pin, PWM_Freq1, 0);
+  ITimer3.init();
+
+  if (ITimer3.attachInterruptInterval(HW_TIMER_INTERVAL_MS, TimerHandler))
+  {
+    Serial.print(F("Starting  ITimer3 OK, micros() = ")); Serial.println(micros());
+  }
+  else
+    Serial.println(F("Can't set ITimer3. Select another freq. or timer"));
+
+#endif
+
+#else
+
+#if USE_TIMER_1
+
+  ITimer1.init();
+
+  // Using ATmega328 used in UNO => 16MHz CPU clock ,
+
+  if (ITimer1.attachInterrupt(HW_TIMER_INTERVAL_FREQ, TimerHandler))
+  {
+    Serial.print(F("Starting  ITimer1 OK, micros() = ")); Serial.println(micros());
+  }
+  else
+    Serial.println(F("Can't set ITimer1. Select another freq. or timer"));
+    
+#elif USE_TIMER_3
+
+  ITimer3.init();
+
+  if (ITimer3.attachInterrupt(HW_TIMER_INTERVAL_FREQ, TimerHandler))
+  {
+    Serial.print(F("Starting  ITimer3 OK, micros() = ")); Serial.println(micros());
+  }
+  else
+    Serial.println(F("Can't set ITimer3. Select another freq. or timer"));
+
+#endif
+
+#endif
+
+  // Just to demonstrate, don't use too many ISR Timers if not absolutely necessary
+  // You can use up to 16 timer for each ISR_PWM
+  //void setPWM(uint32_t pin, uint32_t frequency, uint32_t dutycycle
+  // , timer_callback_p StartCallback = nullptr, timer_callback_p StopCallback = nullptr)
+  Serial.print(F("Using PWM Freq = ")); Serial.print(PWM_Freq); Serial.print(F(", PWM DutyCycle = ")); Serial.println(PWM_DutyCycle);
+
+#if USING_PWM_FREQUENCY
+
+  // You can use this with PWM_Freq in Hz
+  ISR_PWM.setPWM(PWM_Pin, PWM_Freq1, PWM_DutyCycle);
+
+#else
+#if USING_MICROS_RESOLUTION
+  // Or using period in microsecs resolution
+  channelNum = ISR_PWM.setPWM_Period(PWM_Pin, PWM_Period1, PWM_DutyCycle);
+#else
+  // Or using period in millisecs resolution
+  channelNum = ISR_PWM.setPWM_Period(PWM_Pin, PWM_Period1 / 1000.0, PWM_DutyCycle);
+#endif
+#endif
+
+runStartRamp();
 }
 
+////////////////////////////////////////////////
 
+void changePWM(float dutyCycle)
+{
+  static uint8_t count = 1;
 
-void loop() {
-  static float dutyCycle;
-  char receivedChar;
-
-  if (Serial.available() > 0) {
-    receivedChar = Serial.read();
-    if ((receivedChar >= '0') && (receivedChar <= '9')) {
-      dutyCycle = (receivedChar - '0') * 10.0/2.0;
-      changeDutyCycle(dutyCycle);
-    } else if (receivedChar == 'a') {
-      dutyCycle = 100.0;
-      changeDutyCycle(dutyCycle);
-    } else if (receivedChar == '+') {
-      changeDutyCycle(++dutyCycle);
-    } else if (receivedChar == '-') {
-      changeDutyCycle(--dutyCycle);
-    }
+  // You can use this with PWM_Freq in Hz
+  if (!ISR_PWM.modifyPWMChannel(channelNum, PWM_Pin, PWM_Freq, dutyCycle))
+  {
+    Serial.print(F("modifyPWMChannel error for PWM_Period"));
   }
+}
+
+////////////////////////////////////////////////
+
+////////////////////////////////////////////////
+
+void loop()
+{
+
 }
